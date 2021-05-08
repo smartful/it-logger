48aa9352b5564e162625f5fa27c1c3c7af39e4ad
smartful <remi_rodrigues@hotmail.com>
2021-05-08 02:43:41 +0200

    add detail on logItem component

--- src/components/logs/LogItem.jsx
+++ src/components/logs/LogItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Moment from 'react-moment';
 
 
 const LogItem = ({ log }) => {
@@ -11,6 +12,16 @@ const LogItem = ({ log }) => {
           className={`modal-trigger ${log.attention ? 'red-text' : 'blue-text'}`}
         >
           {log.message}
+        </a>
+        <br/>
+        <span className="grey-text">
+          <span className="black-text">ID #{log.id}</span>{' '}
+          last updated by{' '}
+          <span className="black-text">{log.tech}</span>{' '}
+          on <Moment format="MMMM Do YYYY, h:mm:ss a">{log.date}</Moment>
+        </span>
+        <a href="#!" className="secondary-content">
+          <i className="material-icons grey-text">delete</i>
         </a>
       </div>
     </li>
