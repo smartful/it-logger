a7ae2e3d81d844d830605cdc5080d55c3cde434d
smartful <remi_rodrigues@hotmail.com>
2021-05-09 02:38:52 +0200

    adding modals for logs and techs

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import SearchBar from './components/layout/SearchBar';
 import AddBtn from './components/layout/AddBtn';
 import Logs from './components/logs/Logs';
 import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
 
 const App = () => {
   useEffect(() => {
@@ -18,6 +21,9 @@ const App = () => {
       <div className="container">
         <AddBtn />
         <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
+        <TechListModal />
         <Logs />
       </div>
     </Fragment>
---
+++ src/components/techs/AddTechModal.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import M from 'materialize-css/dist/js/materialize.min.js';
+
+const AddTechModal = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+
+
+  const handleSubmit = () => {
+    if (firstName === '' || lastName === '') {
+      M.toast({ html: 'Please enter the first and last name !'})
+    } else {
+      console.log(firstName, lastName);
+
+      // Clear field
+      setFirstName('');
+      setLastName('');
+    }
+  }
+
+  return (
+    <div id="add-tech-modal" className="modal">
+      <div className="modal-content">
+        <h4>New Technician</h4>
+        <div className="row">
+          <div className="input-field">
+            <input
+              type="text"
+              name="firstName"
+              value={firstName}
+              onChange={e => setFirstName(e.target.value)}
+            />
+            <label htmlFor="firstName" className="active">First Name</label>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="input-field">
+            <input
+              type="text"
+              name="lastName"
+              value={lastName}
+              onChange={e => setLastName(e.target.value)}
+            />
+            <label htmlFor="lastName" className="active">Last Name</label>
+          </div>
+        </div>
+      </div>
+
+      <div className="modal-footer">
+        <a
+          href="#!"
+          onClick={handleSubmit}
+          className="modal-close waves-effect blue waves-light btn"
+        >
+          Enter
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default AddTechModal;
